021aaef526c954e04696f9375ca06c79308eeaf7
RickyBGamez <rickyb5@live.ca>
2021-06-19 15:02:54 -0600

    Code cleanup

--- index.js
+++ index.js
@@ -20,28 +20,19 @@ client.registry
 
 client.once("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity(`the server!`, { type: 'WATCHING' }).then().catch(console.error);
+    client.user.setActivity(`the server!`, { type: 'WATCHING' }).catch(console.error);
     client.user.setStatus('online');
 });
-
 client.on("warn", console.warn);
 client.on("error", console.error);
 
-client.on("message", (msg) => {
-    if (client.user.id == msg.author.id) { return; }
-    if (msg.mentions.everyone) { return; }
-    console.log(msg.content);
-})
-
 const sqlite = require("sqlite");
 const sqlite3 = require("sqlite3");
-const { Message } = require("discord.js");
 client.setProvider(
     sqlite.open({ filename: 'database.db', driver: sqlite3.Database })
     .then(db => new Commando.SQLiteProvider(db))
     .catch(console.error)
 );
 
-config = require("./config.json");
-
+const config = require("./config.json");
 client.login(config.token);
