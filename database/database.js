b57a2a002516a00712afdfd9fab51ba5fb7c146a
RickyBGamez <rickyb5@live.ca>
2021-06-20 14:56:46 -0600

    Skeleton Database Handler.

---
+++ database/database.js
@@ -0,0 +1,18 @@
+const sqlite = require("sqlite"), sqlite3 = require("sqlite3");
+const predefinedQueries = [
+    [""] = "",
+]
+
+class DatabaseHandler {
+    #database;
+
+    constructor() {
+        this.#database = sqlite.open({filename: 'database.db', driver: sqlite3.Database}).catch(console.error);
+    }
+
+    /*
+        https://www.npmjs.com/package/sqlite
+    */
+}
+
+module.exports = DatabaseHandler;
