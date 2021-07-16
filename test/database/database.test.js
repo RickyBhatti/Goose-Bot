f62ab3a207293712985c853d66d52dcc782e65ef
RickyBGamez <rickyb5@live.ca>
2021-07-15 22:11:54 -0600

    Add a comment to the database test.

--- test/database/database.test.js
+++ test/database/database.test.js
@@ -3,7 +3,7 @@ const DatabaseHandler = require("../../database/database");
 dbHandler = new DatabaseHandler(null, null);
 
 describe('Database Test', () => {
-    it('should not be null', () => {
+    it('should not be null', () => { // Ensures that the database handler is not null.
         assert.notStrictEqual(dbHandler, null);
     });
 });
